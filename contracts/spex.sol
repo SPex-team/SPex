0110be7fb8a18381c6064d01cea4f8e2a41934e9
mingming.tang <mingmingtang@aliyun.com>
2023-01-29 11:39:58 +0800

    add @zondax/filecoin-solidity

--- contracts/spex.sol
+++ contracts/spex.sol
@@ -1,39 +1,65 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+import "@zondax/filecoin-solidity/contracts/v0.8/MinerAPI.sol";
+import "@zondax/filecoin-solidity/contracts/v0.8/types/MinerTypes.sol";
 
 contract SPEX {
-    
-    struct Miner {
+
+    struct ListMiner {
         string id;
-        address currentOwner;
-        bool isOnSale;
+        address seller;
         uint price;
         uint listTime;
+    }
+
+    struct HistoryRecord {
+        string id;
+        address seller;
+        address buyer;
+        uint price;
         uint dealTime;
     }
 
-    mapping(string => Miner) miners;
+    mapping(string => address) contractMiners;
 
-    function confirmChangeOwnerToSpex(string calldata minerId) public {
+    mapping(string => ListMiner) listMiners;
+
+    mapping (address => uint) name;
+
+    function confirmChangeOwnerToSpex(string calldata minerId, string calldata sign) public {
 
     }
 
     function listMiner(string calldata minerId, uint price) public {
-        Miner memory miner = miners[minerId];
-        require(miner.isOnSale == false, "Miner already in list");
-        miner.isOnSale = true;
-        miner.price = price;
+        // Miner memory miner = miners[minerId];
+        // require(miner.isOnSale == false, "Miner already in list");
+        // miner.isOnSale = true;
+        // miner.price = price;
     }
 
     function changePrice(string calldata minerId, uint price) public {
-        Miner memory miner = miners[minerId];
-        require(miner.isOnSale == true, "Miner not list yet");
-        miner.price = price;
+        // Miner memory miner = miners[minerId];
+        // require(miner.isOnSale == true, "Miner not list yet");
+        // miner.price = price;
+    }
+
+    function changeOwner(string calldata minderId, string calldata newOwner) public {
+
     }
 
 
-    
+    function getListMiners() view public {
 
-    
+    }
+
+
+    function getHistoryRecords() view public {
+
+    }
+
+    function getAvailableBalance(bytes memory target) public returns (int256) {
+        MinerTypes.GetAvailableBalanceReturn memory r = MinerAPI.getAvailableBalance(target);
+        return 123;
+    }
 }
