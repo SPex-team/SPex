71d357c0a3da480e2d070966a924f660972465db
mingming.tang <mingmingtang@aliyun.com>
2023-02-08 11:34:12 +0800

    add test case

--- test/spex.ts
+++ test/spex.ts
@@ -3,6 +3,9 @@ import hre from "hardhat"
 import { time } from "@nomicfoundation/hardhat-network-helpers"
 
 import { initContracts, spex } from "./setup"
+import { ethers } from "ethers"
+
+const minerId = "0x00e807"
 
 describe("Spex", function () {
   beforeEach(async function () {
@@ -23,4 +26,12 @@ describe("Spex", function () {
     // expect(await spex.getContractFilecoinAddress).to.equal(contractFilecoinAddress)
   });
 
+
+  it("listMiner", async function () {
+    // const minerId = "0x00e807"
+    const price = ethers.utils.parseUnits("1")
+    await spex.listMiner(minerId, price)
+    // assert that the value is correct
+    expect(await spex.getListMinerById(minerId).price).to.equal(price)
+  });
 });
