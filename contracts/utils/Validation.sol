78a3d66de1a1415a127e60b0292ed49a79fa2e23
mingming.tang <mingmingtang@aliyun.com>
2023-03-01 17:24:22 +0800

    add utils

---
+++ contracts/utils/Validation.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+import "@zondax/filecoin-solidity/contracts/v0.8/MinerAPI.sol";
+import "@zondax/filecoin-solidity/contracts/v0.8/AccountAPI.sol";
+
+
+contract Validation {
+    mapping(bytes => uint256) private nonces;
+
+    function validateOwner(
+        bytes memory minerAddr,
+        bytes memory signature,
+        address sender
+    ) external {
+        bytes memory ownerAddr = getOwner(minerAddr);
+        bytes memory digest = getDigest(
+            ownerAddr,
+            minerAddr,
+            sender
+        );
+        AccountAPI.authenticateMessage(
+            ownerAddr,
+            AccountTypes.AuthenticateMessageParams({
+                signature: signature,
+                message: digest
+            })
+        );
+        
+        nonces[ownerAddr] += 1;
+    }
+
+    function getSigningMsg(bytes memory minerAddr) external returns (bytes memory) {
+        bytes memory ownerAddr = getOwner(minerAddr);
+        return getDigest(ownerAddr, minerAddr, msg.sender);
+    }
+
+    function getDigest(
+        bytes memory ownerAddr,
+        bytes memory minerAddr,
+        address sender
+    ) private view returns (bytes memory) {
+        bytes32 digest = keccak256(abi.encode(
+            keccak256("validateOwner"),
+            ownerAddr,
+            minerAddr,
+            sender,
+            nonces[ownerAddr],
+            getChainId()
+        ));
+        return bytes.concat(digest);
+    }
+
+    function getOwner(bytes memory minerAddr) private returns (bytes memory) {
+        return MinerAPI.getOwner(minerAddr).owner;
+    }
+
+    function getChainId() private view returns (uint256 chainId) {
+        assembly {
+            chainId := chainid()
+        }
+    }
+}
---
+++ test/spex.ts
@@ -0,0 +1,44 @@
+import { expect } from "chai"
+import hre from "hardhat"
+import { time } from "@nomicfoundation/hardhat-network-helpers"
+
+import { initContracts, spex } from "./setup"
+import { ethers } from "ethers"
+
+const minerId = "0x00e807"
+
+describe("Spex", function () {
+  beforeEach(async function () {
+    await initContracts()
+  })
+
+  it("Should set the right _contractFilecoinAddress", async function () {
+    const contractFilecoinAddress = "0x00da43"
+    await spex.setContractFilecoinAddress(contractFilecoinAddress)
+    // assert that the value is correct
+    expect(await spex.getContractFilecoinAddress).to.equal(contractFilecoinAddress)
+  });
+
+  it("confirmChangeOwnerToSpex", async function () {
+    const minerId = "0x00e807"
+    await spex.getOwnerById(minerId)
+    // assert that the value is correct
+    // expect(await spex.getContractFilecoinAddress).to.equal(contractFilecoinAddress)
+  });
+
+  it("listMiner", async function () {
+    // const minerId = "0x00e807"
+    const price = ethers.utils.parseUnits("1")
+    await spex.listMiner(minerId, price)
+    // assert that the value is correct
+    expect(await spex.getListMinerById(minerId).price).to.equal(price)
+  });
+
+  it("changePrice", async function () {
+    // const minerId = "0x00e807"
+    const price = ethers.utils.parseUnits("2")
+    await spex.changePrice(minerId, price)
+    // assert that the value is correct
+    expect(await spex.getListMinerById(minerId).price).to.equal(price)
+  });
+});
