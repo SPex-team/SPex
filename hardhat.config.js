e658ae0c80a4f9d5ac5b5fb52e34b2cd3b9c8050
mingming.tang <mingmingtang@aliyun.com>
2024-01-08 14:29:43 +0800

    adjust hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,17 +2,9 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  // solidity: "0.8.19",
   solidity: {
     version: "0.8.19",
-    // settings: {
-    //   optimizer: {
-    //     enabled: true,
-    //     runs: 2000,
-    //   },
-    // },
     settings: {
-      viaIR: true,
       optimizer: { enabled: true, runs: 200 },
     },
   },
